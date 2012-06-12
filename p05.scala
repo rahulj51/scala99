160dc71e63b7943ff5e3396027b4c457e88a8a93
Rahul <rahulj51@gmail.com>
2012-06-12 13:34:45 +0800

    practice

---
+++ p05.scala
@@ -0,0 +1,15 @@
+//reverse a list
+
+object P05 {
+
+	def reverse[A](ls : List[A]) : List[A] = ls match {
+	
+		case Nil       => Nil
+		case head :: tail => reverse(tail) ::: List(head) 
+	
+	}
+
+}
+
+var ls = List(1, 1, 2, 3, 5, 8)
+println("reverse of list " + ls + " is " + P05.reverse(ls))
